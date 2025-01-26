3fe890543f019240c25982780ce9b3a70226e476
yes_fisher_bar@25 <shout.prime@gmail.com>
2025-01-26 16:35:08 +0900

    Update main.js

--- Desktop/Tetris/main.js
+++ Desktop/Tetris/main.js
@@ -1,17 +1,5 @@
-// ゲームのエントリーポイント
-// 各モジュールを読み込み、ゲームを起動する
 import { initializeField } from "./field.js";
-import { generateMino } from "./generateMino.js";
 import { gameLoop } from "./gameLoop.js";
-import { isGameOver } from "./gameOver.js";
 
 const field = initializeField(); // フィールドを初期化
-let currentMino = generateMino(); // 最初のミノを生成
-
-gameLoop(field, currentMino); // ゲームループを開始
-
-// ゲームオーバー判定
-if (isGameOver(field)) {
-  alert("Game Over!"); // ゲームオーバーのメッセージを表示
-  location.reload(); // ゲームをリロードして再スタート
-}
+gameLoop(field); // ゲームループを開始
