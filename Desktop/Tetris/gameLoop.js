6ce4a295f94b80a30cdc0c85717ca8c61852b83c
yes_fisher_bar@25 <shout.prime@gmail.com>
2025-01-26 16:45:04 +0900

    Update gameLoop.js

--- Desktop/Tetris/gameLoop.js
+++ Desktop/Tetris/gameLoop.js
@@ -1,38 +1,27 @@
+import { renderField } from "./ui.js"; // UI描画関数をインポート
 import { generateMino } from "./generateMino.js";
 import { checkCollision } from "./collision.js";
 import { is_RowFull } from "./line_clear.js";
-import { isGameOver } from "./gameOver.js";
 
-export function gameLoop(field) {
-  let currentMino = generateMino(); // 最初のミノを生成
+export function gameLoop(field, currentMino, canvasContext) {
+    setInterval(() => {
+        currentMino.y++; // ミノを1段落下
 
-  const interval = setInterval(() => {
-    currentMino.y++; // ミノを1段落下
+        // 衝突チェック
+        if (checkCollision(currentMino, field)) {
+            currentMino.y--; // 衝突した場合は1段戻す
+            lockMino(field, currentMino); // ミノを固定
+            currentMino = generateMino(); // 新しいミノを生成
+            is_RowFull(field); // ラインが埋まっている場合は消去
+        }
 
-    // 衝突チェック
-    if (checkCollision(currentMino, field)) {
-      currentMino.y--; // 衝突した場合は1段戻す
-      lockMino(field, currentMino); // ミノをフィールドに固定
-
-      // ゲームオーバー判定
-      if (isGameOver(field)) {
-        clearInterval(interval); // ゲームループを停止
-        alert("Game Over!"); // ゲームオーバーのメッセージを表示
-        location.reload(); // ゲームをリロードして再スタート
-      }
-
-      currentMino = generateMino(); // 新しいミノを生成
-      is_RowFull(field); // ラインが埋まっている場合は消去
-    }
-  }, 500); // 500msごとに実行
+        // フィールドと現在のミノを描画
+        renderField(field, currentMino, canvasContext);
+    }, 500); // 500msごとに実行
 }
 
-// ミノをフィールドに固定する関数
 function lockMino(field, mino) {
-  const rotation = mino.shape[mino.rotation];
-  for (let block of rotation) {
-    const x = block[0] + mino.x;
-    const y = block[1] + mino.y;
-    field[y][x] = mino.color; // ブロックの位置にミノの色を設定
-  }
+    for (let block of mino.blocks) {
+        field[block.y][block.x] = mino.color; // ブロックの位置にミノの色を設定
+    }
 }
