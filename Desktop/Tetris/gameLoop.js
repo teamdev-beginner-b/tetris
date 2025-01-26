683c22c3f0e4fa5c496f0d89948f4315da839193
yes_fisher_bar@25 <shout.prime@gmail.com>
2025-01-26 16:34:05 +0900

    Update gameLoop.js

--- Desktop/Tetris/gameLoop.js
+++ Desktop/Tetris/gameLoop.js
@@ -1,20 +1,26 @@
-// ゲームループ管理関数
-// 引数:
-// - field: フィールドの状態（ブロックの配置情報）
-// - currentMino: 現在のミノオブジェクト
 import { generateMino } from "./generateMino.js";
 import { checkCollision } from "./collision.js";
 import { is_RowFull } from "./line_clear.js";
+import { isGameOver } from "./gameOver.js";
 
-export function gameLoop(field, currentMino) {
-  // 一定時間ごとにミノを自動的に落下させる
-  setInterval(() => {
+export function gameLoop(field) {
+  let currentMino = generateMino(); // 最初のミノを生成
+
+  const interval = setInterval(() => {
     currentMino.y++; // ミノを1段落下
 
     // 衝突チェック
     if (checkCollision(currentMino, field)) {
       currentMino.y--; // 衝突した場合は1段戻す
-      lockMino(field, currentMino); // ミノを固定
+      lockMino(field, currentMino); // ミノをフィールドに固定
+
+      // ゲームオーバー判定
+      if (isGameOver(field)) {
+        clearInterval(interval); // ゲームループを停止
+        alert("Game Over!"); // ゲームオーバーのメッセージを表示
+        location.reload(); // ゲームをリロードして再スタート
+      }
+
       currentMino = generateMino(); // 新しいミノを生成
       is_RowFull(field); // ラインが埋まっている場合は消去
     }
@@ -22,11 +28,11 @@ export function gameLoop(field, currentMino) {
 }
 
 // ミノをフィールドに固定する関数
-// 引数:
-// - field: フィールドの状態
-// - mino: 固定するミノオブジェクト
 function lockMino(field, mino) {
-  for (let block of mino.blocks) {
-    field[block.y][block.x] = mino.color; // ブロックの位置にミノの色を設定
+  const rotation = mino.shape[mino.rotation];
+  for (let block of rotation) {
+    const x = block[0] + mino.x;
+    const y = block[1] + mino.y;
+    field[y][x] = mino.color; // ブロックの位置にミノの色を設定
   }
 }
