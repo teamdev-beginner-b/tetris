131a025b837bd68fa1261cd0ff6623db7751b3de
yes_fisher_bar@25 <shout.prime@gmail.com>
2025-01-27 11:23:59 +0900

    Update main.js

--- Desktop/Tetris/main.js
+++ Desktop/Tetris/main.js
@@ -3,6 +3,15 @@ import { generateMino } from './generateMino.js';
 import { Field } from './field.js';
 import { handleKeyPress } from './handleKeyPress.js';
 import { is_RowFull, updateScoreDisplay } from './line_clear.js';
+// main.js
+import { blocks } from './mino.js';
+
+// テトリミノの形状と色を表示
+blocks.forEach((block, index) => {
+    console.log(`テトリミノ ${index + 1}:`);
+    console.log('形状:', block.shape);
+    console.log('色:', block.color);
+});
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
