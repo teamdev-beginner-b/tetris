cdad94163a60ad1d7f7434ec6db4d42172728d0a
yes_fisher_bar@25 <shout.prime@gmail.com>
2025-01-27 10:08:57 +0900

    Update block.js

--- Desktop/Tetris/block.js
+++ Desktop/Tetris/block.js
@@ -1,21 +1,15 @@
 // Blockクラス: テトリスの最小単位である1つのブロックを表現するクラス
-class Block {
+export class Block {
   // コンストラクタ: ブロックの位置 (x, y) を初期化する
-  constructor(x, y) {
+  constructor(x, y, color) {
     this.x = x; // ブロックの横位置（列番号）
     this.y = y; // ブロックの縦位置（行番号）
+    this.color = color; // ブロックの色
   }
 
   // drawメソッド: ブロックをキャンバス上に描画する
-  draw() {
-    push(); // 描画設定を保存（他の描画に影響を与えないようにする）
-    let s = width / 12; // ブロックのサイズを計算（キャンバス幅を12で割る）
-
-    // rect関数: キャンバス上に長方形を描画する
-　　// rect(x, y, w, h) は、x, y で左上座標を指定し、w と h で幅と高さ（ここでは正方形の一辺の長さ）を設定する
-
-    rect(s * this.x, s * this.y, s, s); // ブロックを描画 
-
-    pop(); // 描画設定を復元
+  draw(ctx, blockSize) {
+    ctx.fillStyle = this.color; // ブロックの色を設定
+    ctx.fillRect(this.x * blockSize, this.y * blockSize, blockSize, blockSize); // ブロックを描画
   }
 }
