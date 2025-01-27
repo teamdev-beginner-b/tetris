abb84a021fce79782382887f5648fa11e5ca9a35
yes_fisher_bar@25 <shout.prime@gmail.com>
2025-01-27 10:06:01 +0900

    Update main.js

--- Desktop/Tetris/main.js
+++ Desktop/Tetris/main.js
@@ -1,4 +1,4 @@
-import { GameLoop } from './gameLoop.js';
+import { gameLoop } from './gameLoop.js';
 import { generateMino } from './generateMino.js';
 import { Field } from './field.js';
 import { handleKeyPress } from './handleKeyPress.js';
@@ -8,44 +8,11 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 const field = new Field(ctx);
-const gameLoop = new GameLoop(field, ctx);
-const minoGenerator = generateMino();
+let currentMino = generateMino();
 
 document.addEventListener('keydown', (event) => {
-    handleKeyPress(event, field, minoGenerator);
+    currentMino = handleKeyPress(event, currentMino, field);
 });
-
-// ログレベルを定義
-const e = {
-    Info: 1,
-    Warning: 2,
-    Error: 4,
-    Debug: 8
-};
-
-const loglevel = e.Info | e.Warning | e.Error | e.Debug; // すべてのログレベルを有効にする
-
-// ログ出力用のオブジェクト
-const st = {
-    info(...t) {
-        if (loglevel & e.Info) console.info(...t);
-    },
-    warning(...t) {
-        if (loglevel & e.Warning) console.warn(...t);
-    },
-    error(...t) {
-        if (loglevel & e.Error) console.error(...t);
-    },
-    assert(t, ...n) {
-        if (loglevel & e.Debug) {
-            n.unshift(t);
-            console.assert(t, ...n);
-        }
-    },
-    debug(...t) {
-        if (loglevel & e.Debug) console.debug(...t);
-    }
-};
 
 // スコアを更新する関数
 function updateScore(score) {
@@ -58,14 +25,4 @@ function updateUI(score) {
 }
 
 // ゲームループを開始
-gameLoop.start(updateUI);
-
-// フィールドの更新時にline_clear.jsのis_RowFullを呼び出す
-function updateField(stage) {
-    is_RowFull(stage); // 横一列が揃ったら消去し、スコアを更新
-}
-
-// ゲームループ内でupdateFieldを呼び出す
-gameLoop.onUpdate(() => {
-    updateField(field.stage);
-});
+gameLoop(field, currentMino, ctx);
