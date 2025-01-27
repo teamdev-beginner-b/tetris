704b276ae3f708b7ddc3822d03b0748fdb6c7eea
yes_fisher_bar@25 <shout.prime@gmail.com>
2025-01-27 10:04:51 +0900

    Update field.js

--- Desktop/Tetris/field.js
+++ Desktop/Tetris/field.js
@@ -7,15 +7,10 @@ class Block {
   }
 
   // drawメソッド: ブロックをキャンバス上に描画する
-  draw() {
-    push(); // 描画設定を保存（他の描画に影響を与えないようにする）
-    let s = width / 12; // ブロックのサイズを計算（キャンバス幅を12で割る）
-
-    // rect関数: キャンバス上に長方形を描画する
-　　// rect(x, y, w, h) は、x, y で左上座標を指定し、w と h で幅と高さ（ここでは正方形の一辺の長さ）を設定する
-
-    rect(s * this.x, s * this.y, s, s); // ブロックを描画 
-
-    pop(); // 描画設定を復元
-  }f
+  draw(ctx, blockSize) {
+    ctx.fillStyle = "blue"; // ブロックの色を設定
+    ctx.fillRect(this.x * blockSize, this.y * blockSize, blockSize, blockSize); // ブロックを描画
+  }
 }
+
+export default Block;
